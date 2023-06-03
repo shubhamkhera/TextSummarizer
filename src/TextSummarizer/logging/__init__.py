3cd536f8074058869c3a14664897b3f618a64b00
Shubham Khera <shubham_khera@hotmail.com>
2023-06-03 18:10:29 +0530

    test

--- src/TextSummarizer/logging/__init__.py
+++ src/TextSummarizer/logging/__init__.py
@@ -20,7 +20,7 @@ logging_str = "[%(asctime)s: %(levelname)s: %(module)s: %(meassage)s]"
 log_dir = "logs"
 log_filepath = os.path.join(log_dir,"running_logs.log")
 
-os.makedirs(log.dir, exist_ok=True)
+os.makedirs(log_dir, exist_ok=True)
 
 logging.basicConfig(
     level=logging.INFO,
